38cdc0fe26b34dbc4d81e385d1786b6da801799f
Slawek <orlowski.slawomir@mail.canon>
2021-12-30 17:16:58 +0100

    read only properties

--- Fraction.py
+++ Fraction.py
@@ -2,15 +2,32 @@ class Fraction:
     """[summary]
     """
 
+    @staticmethod
+    def one():
+        return Fraction(1, 1)
+
+    @property
+    def numerator(self) -> float:
+        return self._numerator
+
+    @property
+    def denumerator(self) -> float:
+        return self._denominator
+
     def __init__(self, numerator: int = 0, denominator: int = 1):
-        self.numerator = numerator
+        self._numerator = numerator
         if denominator == 0:
             raise ValueError("Denominator must be != 0")
-        self.denominator = denominator
+        self._denominator = denominator
 
     def __str__(self):
-        return str(self.numerator) + "/" + str(self.denominator)
+        return str(self._numerator) + "/" + str(self._denominator)
 
     def __float__(self):
-        return self.numerator / self.denominator
+        return self._numerator / self._denominator
+
+
+v = Fraction(1, 2)
+v._numerator = 10
+print(v)
 
