0e0e20d8c1842c643077092495539250a84341ad
Slawek <orlowski.slawomir@mail.canon>
2021-12-30 16:18:47 +0100

    Basic fraction definition

--- Fraction.py
+++ Fraction.py
@@ -1,2 +1,16 @@
 class Fraction:
-    pass
+    """[summary]
+    """
+
+    def __init__(self, numerator: int = 0, denominator: int = 1):
+        self.numerator = numerator
+        if denominator == 0:
+            raise ValueError("Denominator must be != 0")
+        self.denominator = denominator
+
+    def __str__(self):
+        return str(self.numerator) + "/" + str(self.denominator)
+
+    def __float__(self):
+        return self.numerator / self.denominator
+
